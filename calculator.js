e1d4681bc1c56b3e015f2eeab554aaf745f6406f
Jordan Mata <jordanmata15@yahoo.com>
2023-08-03 13:09:29 -0700

    Create minimal UI without wiring it up
    UI skeleton is completed to give an idea of how the calculator will
    be laid out.

--- calculator.js
+++ calculator.js
@@ -5,6 +5,12 @@ const OPERATIONS = {
     "/": (a, b) => a / b,
 }
 
+const buttons = document.querySelector(".buttons");
+const numericButtonList = Array.from(buttons.querySelectorAll('button.number'))
+                                .sort((a,b) => (+a.textContent > +b.textContent) ? 1: -1);
+const plusMinusButton = buttons.querySelector('.button-plus-minus');
+const decimalButton = buttons.querySelector('.button-decimal');
+
 let leftOperand,
     rightOperand,
     operator;
@@ -12,3 +18,13 @@ let leftOperand,
 function operate(left, right, operator) {
     return OPERATIONS[operator](left, right);
 }
+
+function wireUpButtons() {
+    numericButtonList.forEach( button => {
+        button.addEventListener("click", () => console.log(button.textContent));
+    });
+    plusMinusButton.addEventListener("click", () => console.log(plusMinusButton.textContent));
+    decimalButton.addEventListener("click", () => console.log(decimalButton.textContent));
+}
+
+wireUpButtons();
